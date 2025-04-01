a34c6d1dcff5ce6e924b462b2006c8fa1922bbec
02vicmil <vicmil.millberg@gmail.com>
2025-04-01 23:19:50 +0200

    add util_glm.hpp and glm dependency

---
+++ example/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "util_glm.hpp"
+
+int main()
+{
+    std::cout << "hello world!" << std::endl;
+    return 0;
+}
---
+++ util_glm.hpp
@@ -0,0 +1,9 @@
+
+// ============================================================
+//                           Include
+// ============================================================
+
+#pragma once
+
+#include "glm/glm/gtx/transform.hpp"
+#include "glm/glm/gtx/string_cast.hpp"
