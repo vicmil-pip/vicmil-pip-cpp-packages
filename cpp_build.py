a8c68bfb9c4697e53eb499a7adc2aea385feca53
02vicmil <vicmil.millberg@gmail.com>
2025-04-02 08:27:52 +0200

    add miniz and util file

---
+++ cpp_build.py
@@ -0,0 +1,17 @@
+# Contains information on how to build this package, what to include etc.
+
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parents[0])) 
+sys.path.append(str(Path(__file__).resolve().parents[1])) 
+sys.path.append(str(Path(__file__).resolve().parents[2])) 
+sys.path.append(str(Path(__file__).resolve().parents[3])) 
+sys.path.append(str(Path(__file__).resolve().parents[4])) 
+
+from vicmil_pip.packages.cppBuild import BuildSetup, path_traverse_up
+
+def get_build_setup(browser: bool):
+    new_build_setup = BuildSetup(browser=browser)
+    new_build_setup.n6_include_paths.append(f'-I "{path_traverse_up(__file__, 0)}"')
+    new_build_setup.n2_cpp_files.append(path_traverse_up(__file__, 0) + "/miniz/miniz.c")
+    return new_build_setup
---
+++ example/main.cpp
@@ -0,0 +1,8 @@
+#include "util_std.hpp"
+#include "util_miniz.hpp"
+
+int main()
+{
+    std::cout << "hello world!" << std::endl;
+    return 0;
+}
