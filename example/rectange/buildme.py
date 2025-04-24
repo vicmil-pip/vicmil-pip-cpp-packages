acc8792ae50726933cc14a7c5f2a37dd24232e74
02vicmil <vicmil.millberg@gmail.com>
2025-04-24 04:17:02 +0200

    add minimal rectangle example

--- example/rectange/buildme.py
+++ example/rectange/buildme.py
@@ -5,12 +5,13 @@ sys.path.append(str(Path(__file__).resolve().parents[1]))
 sys.path.append(str(Path(__file__).resolve().parents[2])) 
 sys.path.append(str(Path(__file__).resolve().parents[3])) 
 sys.path.append(str(Path(__file__).resolve().parents[4])) 
+sys.path.append(str(Path(__file__).resolve().parents[5])) 
 
 from vicmil_pip.packages.cppBuild import *
 
 cpp_files = [path_traverse_up(__file__, 0) + "/main.cpp"]
 
-build_setup = BuildSetup(browser=False)
+build_setup = BuildSetup(browser=True)
 build_setup.add_default_parameters(
     cpp_file_paths=cpp_files, 
     output_dir=path_traverse_up(__file__, 0) + "/bin",
