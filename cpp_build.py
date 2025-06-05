7c6032e4f649c53efc29ad6afc0ba3425dd8f619
02vicmil <vicmil.millberg@gmail.com>
2025-06-05 14:59:54 +0200

    fix broken code

--- cpp_build.py
+++ cpp_build.py
@@ -8,37 +8,42 @@ sys.path.append(str(Path(__file__).resolve().parents[2]))
 sys.path.append(str(Path(__file__).resolve().parents[3])) 
 sys.path.append(str(Path(__file__).resolve().parents[4])) 
 
-from vicmil_pip.packages.cppBuild import BuildSetup, path_traverse_up
+from vicmil_pip.packages.cppBuild import BuildSetup, get_directory_path
+
+def get_dependencies(browser: bool):
+    return [
+        "cppBasics",
+    ]
 
 def get_build_setup(browser: bool):
     new_build_setup = BuildSetup(browser=browser)
-    new_build_setup.n6_include_paths.append(f'-I "{path_traverse_up(__file__, 0)}"')
+    new_build_setup.n6_include_paths.append(get_directory_path(__file__, 0))
 
     if not browser:
-        dependencies_directory = path_traverse_up(__file__, 0) + "/socket.io-client"
+        dependencies_directory = get_directory_path(__file__, 0) + "/socket.io-client"
     
         new_build_setup.n2_cpp_files.append(dependencies_directory + "/src/sio_client.cpp")
         new_build_setup.n2_cpp_files.append(dependencies_directory + "/src/sio_socket.cpp")
         new_build_setup.n2_cpp_files.append(dependencies_directory + "/src/internal/sio_client_impl.cpp")
         new_build_setup.n2_cpp_files.append(dependencies_directory + "/src/internal/sio_packet.cpp")
         
-        new_build_setup.n6_include_paths.append(f'-I "{dependencies_directory + "/lib"}"')
-        new_build_setup.n6_include_paths.append(f'-I "{dependencies_directory + "/lib/websocketpp"}"')
-        new_build_setup.n6_include_paths.append(f'-I "{dependencies_directory + "/lib/asio/asio/include"}"')
-        new_build_setup.n6_include_paths.append(f'-I "{dependencies_directory + "/lib/rapidjson/include"}"')
+        new_build_setup.n6_include_paths.append(dependencies_directory + "/lib")
+        new_build_setup.n6_include_paths.append(dependencies_directory + "/lib/websocketpp")
+        new_build_setup.n6_include_paths.append(dependencies_directory + "/lib/asio/asio/include")
+        new_build_setup.n6_include_paths.append(dependencies_directory + "/lib/rapidjson/include")
 
         # These will force ASIO to compile without Boost
-        new_build_setup.n4_macros.append("-DBOOST_DATE_TIME_NO_LIB")
-        new_build_setup.n4_macros.append("-DBOOST_REGEX_NO_LIB")
-        new_build_setup.n4_macros.append("-DASIO_STANDALONE")
+        new_build_setup.n4_macros.append("BOOST_DATE_TIME_NO_LIB")
+        new_build_setup.n4_macros.append("BOOST_REGEX_NO_LIB")
+        new_build_setup.n4_macros.append("ASIO_STANDALONE")
             
         # These will force sioclient to compile with C++11
-        new_build_setup.n4_macros.append("-D_WEBSOCKETPP_CPP11_STL_")
-        new_build_setup.n4_macros.append("-D_WEBSOCKETPP_CPP11_FUNCTIONAL")
-        new_build_setup.n4_macros.append("-D_WEBSOCKETPP_CPP11_TYPE_TRAITS_")
-        new_build_setup.n4_macros.append("-D_WEBSOCKETPP_CPP11_CHRONO_")
+        new_build_setup.n4_macros.append("_WEBSOCKETPP_CPP11_STL_")
+        new_build_setup.n4_macros.append("_WEBSOCKETPP_CPP11_FUNCTIONAL")
+        new_build_setup.n4_macros.append("_WEBSOCKETPP_CPP11_TYPE_TRAITS_")
+        new_build_setup.n4_macros.append("_WEBSOCKETPP_CPP11_CHRONO_")
 
         # Disable sockeio logging
-        new_build_setup.n4_macros.append("-DSIO_DISABLE_LOGGING")
+        new_build_setup.n4_macros.append("SIO_DISABLE_LOGGING")
 
     return new_build_setup
