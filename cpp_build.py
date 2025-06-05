23ba19b7fca8d460e6bab7434fd9ae957e3ed4f9
02vicmil <vicmil.millberg@gmail.com>
2025-06-05 05:49:27 +0200

    move include directory, add additional files for overriding standard thread includes

---
+++ cpp_build.py
@@ -0,0 +1,19 @@
+# Contains information on how to build this package, what to include etc.
+
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parents[0])) 
+sys.path.append(str(Path(__file__).resolve().parents[1])) 
+sys.path.append(str(Path(__file__).resolve().parents[2])) 
+sys.path.append(str(Path(__file__).resolve().parents[3])) 
+sys.path.append(str(Path(__file__).resolve().parents[4])) 
+
+from vicmil_pip.packages.cppBuild import BuildSetup, get_directory_path
+
+import platform
+
+def get_build_setup(browser: bool):
+    new_build_setup = BuildSetup(browser=browser)
+    if platform.system() == "Windows": # Windows
+        new_build_setup.n6_include_paths.append(get_directory_path(__file__, 0) + "/include")
+    return new_build_setup
---
+++ include/future.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "mingw-std-threads/mingw.future.h"
